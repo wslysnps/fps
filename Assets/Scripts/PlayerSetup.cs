4b3c4ceceb2be07f83350c991183a883433c1862
wslysnps <39460015+wslysnps@users.noreply.github.com>
2018-09-13 17:29:23 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
+    Camera sceneCamera;
+
+    void Start ()
+    {
+        if (!isLocalPlayer)
+        {
+            DisableComponenets();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+
+        RegisterPlayer();
+
+    }
+
+    void RegisterPlayer ()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void AssignRemoteLayer ()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponenets ()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
+    void OnDisable ()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
